e97e16544fd0f652731d15450b784570f422be31
Siyuan Ma <sim653@mail.harvard.edu>
2019-04-19 16:31:58 -0400

    add the option to use silhouette strength as internal clustering structure validation metri; close up the additional parameters to pass on to cluster.function and classification as these are now encompassed by all the availabe options

--- R/discrete.discover.R
+++ R/discrete.discover.R
@@ -1,48 +1,54 @@
-#' Main function for discrete structure discovery
+#' Internal evaluation and external validation of discrete clustering structures
 #'
-#' @param distance dissimilarity object
-#' @param batch Name of batch variable
-#' @param data data frame
-#' @param k.max max k for testing
-#' @param cluster.method method for clustering. consistent with fpc
-#' @param classify.method method for classification. consistent with fpc
-#' @param M number of folds validations during internal validation
-#' @param nnk number of k nearest neighbors, if method is KNN
-#' @param diagnostics logical. Should diagnostics be plotted?
-#' @param verbose logical. Verbose?
-#' @param ... additional parameters for clsutering. consistent with fpc
-#' @return A list of MMUPHin continuous score discovery object
+#' @param D a dissimilarity object.
+#' @param batch name of the batch variable.
+#' @param data data frame of metadata, must contain batch.
+#' @param k.max max number of clusters to test for
+#' @param cluster.function clustering function. Should be consistent with fpc, i.e.,
+#' @param internal.metric internal evaluation metric for clustering structures. Currently only
+#' "prediction.strength" and "silhouette.width" are supported.
+#' @param classify.method method for classification (used for external validation and if
+#' internal.metric="prediction.strength"). Currently only "centroid" and "knn" are supported.
+#' @param M number of validation folds (for if internal.metric="prediction.strength").
+#' @param nnk number of k nearest neighbors (for if classify.method="knn").
+#' @param diagnostics should diagnostic plots be generated? Deafault to FALSE.
+#' @param verbose should verbose modelling information should be printed? Default to TRUE.
+#'
+#' @return a list of four summary statistics from internal evaluation and external validation of
+#' clustering structures in the data. These are means and standard errors of
+#' internal and external metrics for each number of cluster-batch combination. So each component
+#' (mean/standard error, internal/external) is a (k.max - 1)*(number of batches) numeric matrix.
 #' @export
-discrete.discover <- function(distance,
+discrete.discover <- function(D,
                               batch,
                               data,
                               k.max = 10,
-                              cluster.method = fpc::claraCBI, # must take distance and k as parameters
+                              cluster.function = fpc::claraCBI,
+                              internal.metric = "prediction.strength",
                               classify.method = "centroid",
-                              M = 10,
+                              M = 30,
                               nnk = 1,
-                              diagnostics = TRUE,
-                              verbose = TRUE,
-                              ...) {
-  ## Ensure data formatts are as expected
-  if(!inherits(distance, "dist")) {
-    stop("distance must be a dissimilarity matrix!")
+                              diagnostics = FALSE,
+                              verbose = TRUE) {
+  # Ensure data formatts are as expected
+  if(!inherits(D, "dist")) {
+    stop("D must be a dissimilarity matrix!")
   }
-  dist.all <- as.matrix(distance)
+  D.all <- as.matrix(D)
   data <- as.data.frame(data, stringsAsFactors = FALSE)
   if(!(batch%in% names(data)))
     stop("Batch/covariate variable not found in data.")
-  ## Data dimensions need to agree with each other
-  if(ncol(dist.all) != nrow(data))
-    stop("Dimensions of distance matrix and metadata table do not agree!")
+  # Data dimensions need to agree with each other
+  if(ncol(D.all) != nrow(data))
+    stop("Dimensions of D matrix and metadata table do not agree!")
 
-  ## Check that sample names agree between the feature and metadata table
-  ## And assign row and column names if emppty
+  # Check that sample names agree between the feature and metadata table
+  # And assign row and column names if emppty
   if(is.null(rownames(data)))
     rownames(data) <- paste0("Sample",1:nrow(data))
-  if(is.null(rownames(dist.all)))
-    colnames(dist.all) <- rownames(dist.all) <- rownames(data)
-  if(any(colnames(dist.all) != rownames(data)))
+  if(is.null(rownames(D.all)))
+    colnames(D.all) <- rownames(D.all) <- rownames(data)
+  if(any(colnames(D.all) != rownames(data)))
     stop("Sample names in feature.count and data don't agree!")
 
   # Check batch variable and identify groups
@@ -61,6 +67,12 @@ discrete.discover <- function(distance,
     message("Set k.max to ", k.max)
   }
 
+  # Check that evaluation metric and classification method are supported
+  if(!(internal.metric %in% c("prediction.strength", "silhouette.width")))
+    stop("Only prediction.strength and silhouette.width metrics are supported!")
+  if(!(classify.method %in% c("centroid", "knn")))
+    stop("Only centroid and knn classification methods are supported!")
+
   # Clustering validation
   stats.internal <- replicate(k.max - 1, list(list()))
   stats.external <- replicate(k.max - 1, list(list()))
@@ -71,8 +83,8 @@ discrete.discover <- function(distance,
     clusterings <- list()
     for(i in 1:n.batch) {
       i.batch <- lvl.batch[i]
-      clusterings[[i]] <- cluster.method(
-        data = as.dist(dist.all[batch == i.batch, batch == i.batch]),
+      clusterings[[i]] <- cluster.function(
+        data = as.dist(D.all[batch == i.batch, batch == i.batch]),
         k = k
       )
     }
@@ -83,23 +95,33 @@ discrete.discover <- function(distance,
       i.batch <- lvl.batch[i]
       if(verbose) message("Study is ", i.batch)
       if(verbose) message("Performing internal validation...")
-      i.result.internal <- fpc::prediction.strength(
-        xdata = as.dist(dist.all[batch == i.batch, batch == i.batch]),
-        Gmin = k,
-        Gmax = k,
-        M = M,
-        clustermethod = cluster.method,
-        classification = classify.method,
-        nnk = nnk,
-        distances = TRUE,
-        count = FALSE
-      )
-      stats.internal[[k - 1]][[i]] <- i.result.internal$predcorr[[k]]
+      if(internal.metric == "prediction.strength") {
+        i.result.internal <- fpc::prediction.strength(
+          xdata = as.dist(D.all[batch == i.batch, batch == i.batch]),
+          Gmin = k,
+          Gmax = k,
+          clustermethod = cluster.function,
+          classification = classify.method,
+          M = M,
+          nnk = nnk,
+          distances = TRUE,
+          count = FALSE
+        )
+        stats.internal[[k - 1]][[i]] <- c("mean" = mean(i.result.internal$predcorr[[k]]),
+                                          "sd" = sd(i.result.internal$predcorr[[k]]))
+      }
+      if(internal.metric == "silhouette.width") {
+        sil.width <- cluster::silhouette(
+          clusterings[[i]],
+          dist = as.dist(D.all[batch == i.batch, batch == i.batch]))[, "sil_width"]
+        stats.internal[[k - 1]][[i]] <- c("mean" = mean(sil.width),
+                                          "sd" = sd(sil.width))
+      }
 
       if(verbose) message("Performing external validation...")
       i.clustering <- rep(-1, nrow(data))
       i.clustering[batch == i.batch] <- clusterings[[i]]$partition
-      classifications[[i]] <- fpc::classifdist(as.dist(dist.all),
+      classifications[[i]] <- fpc::classifdist(as.dist(D.all),
                                                clustering = i.clustering,
                                                method = classify.method,
                                                centroids = clusterings[[i]]$result$medoids,
@@ -121,39 +143,37 @@ discrete.discover <- function(distance,
       }
       i.result.external <- apply(i.result.external[setdiff(1:n.batch, i), ],
                                  1, min)
-      stats.external[[k - 1]][[i]] <- i.result.external
+      stats.external[[k - 1]][[i]] <- c(mean = mean(i.result.external),
+                                        sd = sd(i.result.external))
     }
   }
 
+  # If required, generate diagnostic plots
   if(diagnostics) {
-    data.internal <- Reduce("rbind",
-                            lapply(1:(k.max - 1),
-                                   function(i.k) {
-                                     data.frame(k = i.k + 1,
-                                                Reduce("rbind",
-                                                       lapply(1:n.batch,
-                                                              function(i)
-                                                                data.frame(pred.strength = stats.internal[[i.k]][[i]],
-                                                                           batch = lvl.batch[i]))))
-                                   }))
-    data.external <- Reduce("rbind",
-                            lapply(1:(k.max - 1),
-                                   function(i.k) {
-                                     data.frame(k = i.k + 1,
-                                                Reduce("rbind",
-                                                       lapply(1:n.batch,
-                                                              function(i)
-                                                                data.frame(pred.strength = stats.external[[i.k]][[i]],
-                                                                           batch = lvl.batch[i]))))
-                                   }))
-    data.plot <- rbind(data.frame(data.internal, validation = "Internal"),
-                       data.frame(data.external, validation = "External"))
-    print(ggplot2::ggplot(data.plot, ggplot2::aes(x = as.factor(k), y = pred.strength)) +
-            ggplot2::geom_boxplot(ggplot2::aes(color = validation)) +
-            ggplot2::facet_grid(.~batch) +
-            ggplot2::theme_bw())
+    diagnostics.discrete.discover(stats.internal = stats.internal,
+                                  stats.external = stats.external,
+                                  lvl.batch = lvl.batch)
   }
-  return(list(internal = stats.internal,
-              external = stats.external))
+
+  # compile results for output
+  internal.mean <- t(sapply(stats.internal,
+                            sapply(k.stats, function(i.stat) i.stat["mean"])))
+  internal.se <- t(sapply(stats.internal,
+                          sapply(k.stats, function(i.stat) i.stat["sd"])))
+  external.mean <- t(sapply(stats.external,
+                            sapply(k.stats, function(i.stat) i.stat["mean"])))
+  external.se <- t(sapply(stats.external,
+                          sapply(k.stats, function(i.stat) i.stat["sd"])))
+  dimnames(internal.mean) <-
+    dimnames(internal.se) <-
+    dimnames(external.mean) <-
+    dimnames(external.se) <- list(as.character(2:k.max),
+                                  lvl.batch)
+
+
+  return(list(internal.mean = internal.mean,
+              internal.se = internal.se,
+              external.mean = external.mean,
+              external.se = external.se))
 }
 
