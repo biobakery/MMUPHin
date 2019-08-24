6b7f5cf72448f473edf6576ebe9a10761f883006
Siyuan Ma <sim653@mail.harvard.edu>
2019-08-24 16:22:16 -0400

    add tests

---
+++ tests/testthat/test-helpers_adjust_batch.R
@@ -0,0 +1,9 @@
+test_that("construct_design works", {
+  expect_equal(construct_design(NULL), NULL)
+
+  df_test <- data.frame(a = 1:3)
+  expect_equal(ncol(construct_design(df_test)), 1)
+
+  df_test <- data.frame(a = factor(1:3))
+  expect_equal(ncol(construct_design(df_test)), 3)
+})
